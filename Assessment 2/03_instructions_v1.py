3584f5435a178c9015ab8f9c7bf74496e3263c08
Albkset <83570540+Albkset@users.noreply.github.com>
2021-05-24 10:51:46 +1200

    instructions1test

---
+++ Assessment 2/03_instructions_v1.py
@@ -0,0 +1,30 @@
+# Functions go here
+def yes_no(question):
+    valid = False
+    while not valid:
+        response = input(question).lower()
+
+        # If they say yes, output 'program continues'
+        if response == "yes" or response == "y":
+            response = "yes"
+            return response
+
+        elif response == "no" or response == "n":
+            response = "no"
+            return response
+
+
+        # If they say no, output 'display instructions'
+        else:
+            print("Please answer yes / no")
+
+# Main Routine goes here
+show_instructions = yes_no("Have you played the game before  ")
+if show_instructions == "yes":
+    print("Programs continues")
+else:
+    print("Diplay instructions")
+
+print()
+having_fun = yes_no("Are you having fun? ")
+print("you said {} to having fun ".format(having_fun))
